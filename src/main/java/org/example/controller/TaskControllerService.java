cefedffb22faaee97531fe7e2fabae482a4dca27
Juan David Carvajal Zapata <jcarvaja@bancolombia.com.co>
2025-06-04 12:10:35 -0500

    cambio del controlador para usarlo por BD h2

--- src/main/java/org/example/controller/TaskControllerService.java
+++ src/main/java/org/example/controller/TaskControllerService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/tasks")
 public class TaskControllerService {
 
     private TaskService service = new TaskService();
